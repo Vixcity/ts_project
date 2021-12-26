8e508074557083dc201f53ec997a7c6fda47db6d
Vixcity <2091283625@qq.com>
2021-12-26 15:43:06 +0800

    12/26 学习ts

---
+++ day_02/变量交换.ts
@@ -0,0 +1,15 @@
+let num: number = 12;
+let num2: number = 32;
+
+[num,num2] = [num2,num];
+console.log("num:",num,'\nnum2:',num2);
+
+let num3: number = 12;
+let num4: number = 32;
+let template: number;
+
+template = num3
+num3 = num4
+num4 = template
+
+console.log(`\ntemplate:${template}\nnum3:${num3}\n,num4:${num4}`)
