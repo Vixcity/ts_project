d97985deb3b69f702a4529b3e63f33c90ec6e30e
vixcity <2091283625@qq.com>
2021-12-28 13:02:46 +0800

    三元运算符

---
+++ day_03/03-01.ts
@@ -0,0 +1,10 @@
+console.log(1+2)   // 3 加
+console.log(2-1)   // 1 减
+console.log(2*2)   // 4 乘
+console.log(4/2)   // 2 除
+
+console.log('周杰'+'伦')
+
+console.log(1+2)   // 3
+console.log(1+'2') // 12
+console.log('1'+2) // 12
